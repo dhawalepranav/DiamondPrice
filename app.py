1f8a638f4b24add62c5383e8964181b89f066463
dhawalepranav <pranavdhawale1998@gmail.com>
2023-04-27 17:47:13 +0530

    web app

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,jsonify
+from src.logger import logging
+from src.exception import CustomException
+from src.pipeline.prediction_pipeline import PredictionPipeline,PredictionPipelineDataPrepare
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template ('index.html')
+
+@app.route('/predictions',methods=['GET','POST'])
+def predictions():
+    if request.method == 'GET':
+        return render_template('form.html')
+    
+    else:
+        data=PredictionPipelineDataPrepare(
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        
+        final_new_data = data.dataframegeneration()
+        predict_pipeline = PredictionPipeline()
+        pred = predict_pipeline.predict(final_new_data)
+
+        results=round(pred[0],2)
+
+        return render_template('result.html',final_result=results)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
